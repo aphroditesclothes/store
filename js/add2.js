86ae2614d0c94db3dc88aac8cc91c333bdd00cdd
aphroditesclothes <118008352+aphroditesclothes@users.noreply.github.com>
2022-11-15 12:41:54 +0700

    Add files via upload

--- js/add2.js
+++ js/add2.js
@@ -135,3 +135,20 @@ const display = () => {
 }
 
 display();
+
+window.onbeforeunload = function (e) {
+
+    window.localStorage.unloadTime = JSON.stringify(new Date());
+    
+};
+
+window.onload = function () {
+    let loadTime = new Date();
+    let unloadTime = new Date(JSON.parse(window.localStorage.unloadTime));
+    let refreshTime = loadTime.getTime() - unloadTime.getTime();
+
+    if(refreshTime > 10000)//3000 milliseconds
+    {
+        window.localStorage.clear();
+    }
+}
